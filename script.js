525055d2a1a7d57a6bab0d32d378f5fe5421cf96
Reneezzzzz <112042893+Reneezzzzz@users.noreply.github.com>
2023-01-06 13:06:48 +1100

    2

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll(".nav-link-scroll").forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+const about = document.querySelector(".about");
+const initalCoords = about.getBoundingClientRect();
+const nav = document.querySelector(".nav-right");
+console.log(nav);
+window.addEventListener("scroll", function () {
+  if (window.scrollY > initalCoords.top - 100) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
